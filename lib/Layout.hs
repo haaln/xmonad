76fab74be4aff2881957f07d942b003cf2bbe75a
haaln <hassanan@kth.se>
2022-12-14 18:31:18 +0100

    modified:   lib/Layout.hs

--- lib/Layout.hs
+++ lib/Layout.hs
@@ -43,26 +43,22 @@ myLayoutHook = avoidStruts
              where
                myDefaultLayout =         
                                         tall
-                                 |||  noBorders tabs
-                                 -- ||| monocle
-                                 -- ||| noBorders monocle
-                                 -- ||| floats
-                                 -- ||| grid
+                                    ||| noBorders tabs
                                  -- ||| spirals
                                  -- ||| threeCol
                                  -- ||| threeRow
+                                 -- ||| noBorders monocle
+                                 -- ||| floats
+                                 -- ||| monocle
+                                 -- ||| grid
                                  -- ||| magnifyy
 
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
--- Below is a variation of the above except no borders are applied
--- if fewer than two windows. So a single window has no gaps.
 mySpacing' :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
--- somehow deleting $ addTabs $ subLayout $ limitWindows sets the correct theme for tabs
--- Defining a bunch of layouts, many that I don't use.
 tall     = renamed [Replace "stack"]
            $ windowNavigation
            $ addTabs shrinkText myTabTheme
@@ -70,49 +66,42 @@ tall     = renamed [Replace "stack"]
            $ limitWindows 12
            $ mySpacing mySpacingWidth
            $ ResizableTall 1 (3/100) (1/2) []
+tabs     = renamed [Replace "tabs"]
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ limitWindows 8
+           $ tabbed shrinkText myTabConfig
+spirals  = renamed [Replace "spirals"]
+           $ subLayout [] (smartBorders Simplest)
+           $ mySpacing' mySpacingWidth
+           $ spiral (6/7)
 magnifyy  = renamed [Replace "magnify"]
-         --  $ addTabs shrinkText myTabTheme
            $ subLayout [] (smartBorders Simplest)
            $ magnifier
            $ limitWindows 12
            $ mySpacing 8
            $ ResizableTall 1 (3/100) (1/2) []
 monocle  = renamed [Replace "monocle"]
-         --  $ windowNavigation
            $ subLayout [] (smartBorders Simplest)
            $ limitWindows 20 Full
 floats   = renamed [Replace "floats"]
            $ limitWindows 20 simplestFloat
 grid     = renamed [Replace "grid"]
-          -- $ windowNavigation
-          -- $ addTabs shrinkText myTabTheme
-          -- $ subLayout [] (smartBorders Simplest)
+           $ subLayout [] (smartBorders Simplest)
            $ limitWindows 12
            $ mySpacing mySpacingWidth
            $ mkToggle (single MIRROR)
            $ Grid (16/10)
-spirals  = renamed [Replace "spirals"]
-           -- $ windowNavigation
-           -- $ addTabs shrinkText myTabTheme
-           -- $ subLayout [] (smartBorders Simplest)
-           $ mySpacing' mySpacingWidth
-           $ spiral (6/7)
 threeCol = renamed [Replace "threeCol"]
-           -- $ windowNavigation
-           -- $ addTabs shrinkText myTabTheme
            $ subLayout [] (smartBorders Simplest)
            $ limitWindows 7
            $ mySpacing mySpacingWidth
            $ ThreeCol 1 (3/100) (1/2)
 threeRow = renamed [Replace "threeRow"]
-           -- $ windowNavigation
-           -- $ addTabs shrinkText myTabTheme
            $ subLayout [] (smartBorders Simplest)
            $ limitWindows 7
            $ mySpacing mySpacingWidth
            $ Mirror
            $ ThreeCol 1 (3/100) (1/2)
-tabs     = renamed [Replace "tabs"]
-           $ tabbed shrinkText myTabConfig
 
 

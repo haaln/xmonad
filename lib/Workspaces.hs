50d6f608788bcd9e7bc9dd5b99eac0dadeefcad5
haaln <hassan@alnoori.group>
2025-06-12 06:03:28 +0200

    reduce workspaces

--- lib/Workspaces.hs
+++ lib/Workspaces.hs
@@ -7,17 +7,9 @@ term = "\xf120"
 file = "\61564"
 dev = "\xf15c"
 misc = "\xf2d2"
-win = "\61818"
-tex = "\xf02d"
-kvm = "\62599"
-game = "\63381"
 
-myWorkspace = [www, term, file, dev, misc, tex, win, kvm, game]
--- windows logo "\61818"
--- mail logo "\64239" or "\63213"
--- git arrow "\61556" or branch "\62488"
--- shell "\61729"
---穀禎﬐
+myWorkspace = [www, term, file, dev, misc]
+
 xmobarEscape :: String -> String
 xmobarEscape = concatMap doubleLts
   where
